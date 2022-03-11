be185548b1e46f7d9bbc737fd94c4a42a3f4deca
Niku Nitin <nn16ms077@gmail.com>
2022-03-11 09:25:19 +0530

    Remove - comment line

--- .config/nvim/lua/nick/core/init.lua
+++ .config/nvim/lua/nick/core/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.load = function()
-	--require "nick.core.nvim_tree".setup()
 	require "nick.core.tokyonight".setup()
 	require "nick.core.lualine".setup()
 	require "nick.core.nvim_cmp".setup()
